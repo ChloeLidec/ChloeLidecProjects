12d30f2acbe760184877d28abceab19256d29e68
ChloeLidec <chloe.lidec@etu.univ-orleans.fr>
2022-12-30 22:54:47 +0100

    maj site flask 30/12 (genres, authors,books,users AESD)

--- SiteFlask/myappwtf/commands.py
+++ SiteFlask/myappwtf/commands.py
@@ -1,4 +1,5 @@
 import click
+
 from .app import app, db
 @app.cli.command()
 @click.argument('filename')
@@ -10,7 +11,7 @@ def loaddb(filename):
     import yaml
     books = yaml.safe_load(open(filename))
     # import des modèles
-    from .models import Author, Book
+    from .models import Author, Book, Genre, BookGenre
     # première passe : création de tous les auteurs
     authors = {}
     for b in books :
@@ -20,7 +21,22 @@ def loaddb(filename):
             db.session.add(o)
             authors[a] = o
     db.session.commit()
-    # deuxième passe : création de tous les livres
+    # deuxième passe : création de tous les genres
+    genres = {}
+    for b in books :
+        if "genres" in b :
+            for g in b["genres"] :
+                if g not in genres :
+                    gen = Genre(name=g)
+                    db.session.add(gen)
+                    genres[g] = gen
+        else:
+            if "Unknown" not in genres:
+                gen=Genre(name="Unknown")
+                db.session.add(gen)
+                genres["Unknown"] = gen
+    db.session.commit()
+    # troisième passe : création de tous les livres
     for b in books :
         a = authors [b["author"]]
         o = Book(price = b["price"],
@@ -29,6 +45,12 @@ def loaddb(filename):
                 img = b["img"] ,
                 author_id = a.id)
         db.session.add(o)
+        db.session.commit()
+        i=0
+        for gen_n,gen in genres.items():
+            book_genre = BookGenre(id=i,book_id=o.id, genre_id=gen.id)
+            db.session.add(book_genre)
+            i+=1
     db.session.commit()
 @app.cli.command()
 def syncdb():
